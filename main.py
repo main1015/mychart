61fefcb19d34aa6a746be5abbd086c068d5870fa
汪余 <wangyu@people.cn>
2023-04-23 14:30:28 +0800

    2

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time: 2023/4/23 11:11 AM
+@Author: wangyu
+"""
+
+__author__ = "wangyu"
+import streamlit as st
+# from loguru import logger
+#
+#
+# @logger.catch
+# def div(a, b):
+#     c = a / b
+#     return c
+#
+#
+# if __name__ == '__main__':
+#     div(2, 0)
+import streamlit as st
+
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
